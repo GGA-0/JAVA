22da087f296e501df809732b05b6f2be394163a9
GGA <102376817+GGAx1000@users.noreply.github.com>
2022-11-08 16:40:54 -0300

    Add files via upload

---
+++ Programs/RPG/JavaRPG/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static int x = 1;
+    public static void main(String[] args) {
+        x += 1;
+        System.out.println(x);
+        testt();
+    }
+    public static void testt(){
+        System.out.println(x);
+    }
+}
