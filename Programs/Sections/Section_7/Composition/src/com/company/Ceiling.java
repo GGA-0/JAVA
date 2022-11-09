65fc947b76109c5f60109bfebb7a347299e24473
GGAx1000 <ggaxmail@gmail.com>
2022-11-09 09:49:39 -0300

    Composition and RPG advanced

--- Programs/Sections/Section_7/Composition/src/com/company/Ceiling.java
+++ Programs/Sections/Section_7/Composition/src/com/company/Ceiling.java
@@ -1,2 +1,25 @@
-package com.company;public class Ceiling {
+package com.company;
+//  3) Create a class with the name Ceiling.
+//  There are two member variables for this one:
+//  height and paintedColor, both of type int.
+//  There should also be a constructor which accepts both member variables as parameters.
+
+//  There are also two additional methods which should be defined:
+//  getHeight() shall return the value of height and getPaintedColor() should return the value of paintedColor.
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
 }
