98c85a71c39ce48e8d0c9fd618718803e6babf05
GGAx1000 <ggaxmail@gmail.com>
2022-11-09 11:57:22 -0300

    .

--- Programs/RPG/JavaRPG/test.java
+++ Programs/RPG/JavaRPG/test.java
@@ -1,11 +1,7 @@
+import javax.swing.JFrame;
+
 public class test {
-    public static int x = 1;
     public static void main(String[] args) {
-        x += 1;
-        System.out.println(x);
-        testt();
-    }
-    public static void testt(){
-        System.out.println(x);
+        RPGframe frame1 = new RPGframe();
     }
 }
